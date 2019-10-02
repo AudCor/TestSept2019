f560940e39bd38fa62df2c985b7c5b63c912f547
Aud'Cor <51374240+AudCor@users.noreply.github.com>
2019-10-02 11:38:16 +0200

    Premier exercice node.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk');
+
+console.log('Voici 4 wilders trop cool : ' + chalk.cyan('Hervé') + ' ' + ' ' + chalk.yellow('Momo') + ' ' + ' ' + chalk.magenta('Hadi') + ' ' + ' ' + chalk.green('Andi'));
+
